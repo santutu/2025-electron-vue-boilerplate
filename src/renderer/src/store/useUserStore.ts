403852a2199007a45ec322394576b19a7abd9382
santutu <santutu@naver.com>
2025-07-04 23:53:09 +0900

    hmr for store

--- src/renderer/src/store/useUserStore.ts
+++ src/renderer/src/store/useUserStore.ts
@@ -1,6 +1,7 @@
 import { User } from '@renderer/models/User'
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import { acceptHMRUpdate } from 'pinia'
 
 export const useUserStore = defineStore('user', () => {
   const user = ref<User>({
@@ -10,3 +11,8 @@ export const useUserStore = defineStore('user', () => {
 
   return { user }
 })
+
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useUserStore, import.meta.hot))
+}
